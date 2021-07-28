c2a0e165a36fcf59335f54f29e42aa23593877f3
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-28 12:22:19 +0900

    input에 onChange 이벤트 생성

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+
+
     render() {
        return (
           <div className="ui segment">
             <form className="ui form">
               <div className="field">
                 <label>Image Search</label>
-                <input type="Text" />
+                   <input
+                      type="Text"
+                      onChange={(e) => console.log(e.target.value)}
+                  //arrow function
+                   />
                </div>
             </form>
           </div>
